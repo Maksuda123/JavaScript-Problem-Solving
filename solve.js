50aaae61c707d174cf5222c3798991d82b304306
Maksuda Mila <48387926+Maksuda123@users.noreply.github.com>
2021-12-27 01:33:29 +0600

    JsProblemSolve From LearnWithSumit

--- solve.js
+++ solve.js
@@ -1,573 +1,185 @@
-//1. Factorial
-
-function firstFactorial(num) {
-  return num === 1 ? 1 : num * firstFactorial(num - 1);
+//1.Chokka rsndomly print
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
+console.log(getRandomNumber(1, 6));
 
-console.log(firstFactorial(5));
 
-/////////////////////////////////////////////////////////
+//2.Name print Alphabetically
+const names = ['Mila', 'Shila', 'Nila'];
+console.log(names.sort());
+//aray.sort main array k change kore fele.
 
-//2. reverse string
 
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
+//3.Number serially Shajabo
+const numbers = [1, 4, 2, 9, 5, 3, 8];
 
-console.log(reverseString("Maksuda Mila"));
+const result = numbers.sort((a, b) => a - b);
+console.log(result);
 
-//Array reverse
-const array1 = ["one", "two", "three"];
-const reversed = array1.reverse();
-console.log("reversed:", reversed);
-//output: reversed:['three', 'two', 'one']
 
-//string split
-const str = "How are you";
-const stringSplit = str.split("").reverse("").join("");
-console.log(stringSplit);
-//output: uoy era woH
-
-//////////////////////////////////////////
-
-//2. reverse string way No 2.
-function firstReverse(str) {
-  const reverseArray = [];
-  for (i = str.length - 1; i > -1; i--) {
-    reverseArray.push(str[i]);
-  }
-  str = reverseArray.join("");
-  return str;
-}
-console.log(firstReverse("Maksuda Mila"));
-
-//////////////////////////////////////////
-
-//3. user name Validation
-
-function usernameValidation(str) {
-  const regExpression = /^[a-zA-Z]\w+[A-Za-z0-9]$/;
-
-  const length = (str) => str.length >= 4 && str.length <= 25;
-  return regExpression.test(str) && length(str);
-}
-
-console.log(usernameValidation("u__hello_world123"));
-//output: true
-
-/////////////////////////////////////////////
-
-//4. Factorial
-function factorial(n) {
-  return n === 1 ? 1 : factorial(n - 1);
-}
-
-const fact = factorial(4);
-console.log(fact);
-
-//////////////////////////////////////////
-
-//5. BracketCombibation
-
-function bracketCombibation(num) {
-  let factorial = (n) => {
-    let k = 1;
-    for (var i = n; i >= 1; i--) {
-      k *= i;
+//4. Leap Year ki na ber koro?
+function isLeapYear(year) {
+    if ((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))) {
+        console.log(`${year} is a leap year!`);
+    } else {
+        console.log(`${year} is not a leap year`)
     }
-    return k;
-  };
-  const result = factorial(2 * num) / (factorial(num + 1) * factorial(num));
-  return result;
 }
-const finalResult = bracketCombibation(5);
-console.log(finalResult);
+isLeapYear(2028)
 
-//////////////////////////////////////////
 
-//6. Find Intersection
-function FindIntersection(strArr) {
-  const arrayOne = strArr[0].split(", ");
-  const arrayTwo = strArr[1].split(", ");
-  let result = arrayOne.filter((arrTwo) =>
-    arrayTwo.find((arrOne) => arrOne === arrTwo)
-  );
-  return result.length > 0 ? result.join(",") : "false";
-}
 
-const myArray = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"];
-console.log(myArray);
-const finalResult = FindIntersection(["1, 3, 5, 10, 17, 18", "2, 4, 9, 4"]);
-console.log(finalResult);
-//output: False
+//5. Kono sentence a vowel ache koto gulo ber koro
 
-//////////////////////////////////////////
-
-//7. Questions Marks
-function QuestionsMarks(str) {
-  res = false;
-  for (let i = 0; i < str.length; i++) {
-    for (let j = i + 1; j < str.length; j++) {
-      if (Number(str[i]) + Number(str[j]) === 10) {
-        res = true;
-        if (str.slice(i, j).split("?").length - 1 < 3) {
-          return false;
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", 'U'];
+function findVowel(sentance) {
+    let count = 0;
+    const letters = Array.from(sentance);//array banalam
+    letters.forEach(function(value) {
+        if (vowels.includes(value)) {
+            count++;
         }
-      }
-    }
-  }
-  return res;
+    })
+    return count;
 }
-console.log(QuestionsMarks("acc?7??sss?3rr1??????5"));
+console.log(findVowel("Hello I am Mila!"))
 
-//////////////////////////////////////////
 
-//8. Tree Constructor
-function TreeConstructor(strArr) {
-  let parents = {};
-  let children = {};
 
-  for (let i = 0; i < strArr.length; i++) {
-    //"(1,2)" => ["1", "2"]
-    let pair = strArr[i].replace(/[()]/g, "").split(",");
-    let child = pair[0];
-    let parent = pair[1];
+//6. Ekta array theke duplicate value gula bar kore anbo.
 
-    if (parents[parent]) {
-      parents[parent].push(child);
-      console.log(parents[parent]);
-    } else {
-      parents[parent] = [child];
+const numbers = [1, 1, 2, 3, 4, 6, 6, 2];
+const duplicates = numbers.filter(function (value, index, array) {
+    return array.indexOf(value) !== index;
+})//indexof prothom index pai shob shomoi
+console.log(duplicates);
+
+
+
+//7. Ekta array theke unique value gula bar kore anbo.
+const numbers = [1, 1, 2, 3, 4, 6, 6, 2];
+const duplicates = numbers.filter(function (value, index, array) {
+    return array.indexOf(value) === index;
+})//indexof prothom index pai shob shomoi
+console.log(duplicates);
+
+
+//8. Ekta boro sentance theke 'sumit' shobdo ti koto ase ber koro. Prothom bar Saha koto number position a ase?
+const sentence = "Sumit Saha is a Tech Entrepreneur who Founded Analyzen - the first ever Digital Agency in Bangladesh. He is specialized in Full Stack Web Development.  Learn with Sumit is all about teaching web development skills and techniques in an efficient and practical manner. If you are just getting started in web development, Learn with Sumit has all the tools you need to learn the newest and most popular technologies to convert you from a no stack to full stack developer. Learn with Sumit also deep dives into advanced topics using the latest best practices for you seasoned web developers.";
+
+
+const matches = sentence.match(/sumit/ig);
+const occurances = matches ? matches.length : 0;
+console.log(occurances);
+
+let position = sentence.search(/sumit/i);
+position >= 0 ? position : 'Not found';
+console.log(position);
+
+
+//9. input linearSearch(['a', 'b', 'c', 'd', 'e' ], 'c')
+//output: 2 or 'Not found'
+const linearSearch = (arr, val) => {
+    let length = arr.length;
+    for (i = 0; i <= length; i++){
+        if (arr[i] === val) {
+            return i;
+        }
     }
-    if (parents[parent].length > 2) {
-      return false;
-    }
-    if (children[child]) {
-      return false;
-    } else {
-      children[child] = parent;
-    }
-  }
-  return true;
+    return 'Not found';
 }
-console.log(TreeConstructor(["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"]));
+const result = linearSearch(["a", "b", "c", "d", "e"], "c");
 
-////////////////////////////////////////////
+console.log(result);
 
-//9. minimum window substring
 
-function MinWindowSubstring(strArr) {
-  let str = strArr[0];
-  let needle = strArr[1].split("");
 
-  //start with the smallest possible substrings, then go up
-  for (let i = needle.length, len = str.length; i <= len; i++) {
-    for (j = 0; j <= len - i; j++) {
-      let mySlice = str.substr(j, i);
-      if (isContained(mySlice)) {
-        return mySlice;
-      }
+//10. Kono array theke shob theke boro string khuje ber koro & tar index number dekhaw.
+const longestString = (array) => {
+    let longestWord = '';
+
+    for (element of array) {
+        if (element.length > longestWord.length) {
+            longestWord = element;
+        }
     }
-  }
-  return "Not in string";
+    return [ longestWord, array.indexOf(longestWord)];
+}
 
-  //isContained checks to see if all the chars in the needle are in the given string
-  function isContained(str) {
-    let arr = str.split("");
-    for (let i = 0, len = needle.length; i < len; i++) {
-      let place = arr.findIndex((val) => {
-        return val === needle[i];
-      });
-      if (place === -1) {
+const result = longestString(['Sumit Saha', 'Learn with Sumit', 'Maksuda Mila'])
+console.log(result);
+
+
+//11. 1-100 porjonto value gulo 3 or 5 abong 3, 5 ovoi value dhara bibajjo ki na?
+
+function fixxBuzz(number) {
+    for (let i = 1; i <= number; i++){
+        if (i % 15 === 0) {
+          console.log(`${i} is FizzBuzz`);
+        } else if (i % 3 === 0) {
+          console.log(`${i} is Fizz`);
+        } else if (i % 5 === 0) {
+          console.log(`${i} is Buzz`);
+        }
+    }
+}
+
+console.log(fixxBuzz(100))
+
+
+
+//12. Array theke falsy value khuje bad dibo.
+const mixdArray = [
+    'lws',
+    undefined,
+    'learn with sumit',
+    false,
+    "",
+    'apple',
+    40,
+    "k",
+    true,
+    "Thanks All",
+    NaN
+];
+
+const trueArray = mixdArray.filter((el) => {
+    if (el) {
+        return true;
+    } else {
         return false;
-      } else {
-        arr.splice(place, 1);
-      }
     }
-    return true;
-  }
-}
-
-MinWindowSubstring();
-
-//////////////////////////////////////
-
-//10. Check if a number is even
-// (10);
-// Expected;
-// true;
-// (-4);
-// Expected;
-
-function myFunction(num) {
-  if (num % 2 == 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-const results = myFunction(10);
-console.log(results);
-
-///////////////////////////////////
-
-//11. How many times does a character occur ?
-// Write a function that takes a two strings as arguments
-// Return the number of times the first string occurs in the second string
-
-// ("m", "How many times does the character occur in this sentence?");
-// Expected;
-// 2;
-// ("h", "How many times does the character occur in this sentence?");
-// Expected;
-// 4;
-
-const characterCount = (letter, str) => {
-  let letterCount = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) == letter) {
-      letterCount += 1;
-    }
-  }
-  return letterCount;
-};
-console.log(
-  characterCount(
-    "m",
-    "How many times does the character occur in this sentence?"
-  )
-);
-
-//////////////////////////////////////////////
-
-//12. Check if a number is a whole number
-// Write a function that takes a number as argument
-// If the number is a whole number (has no decimal place), return true
-// Otherwise, return false
-
-// (4);
-// Expected;
-// true;
-// (1048);
-// Expected;
-// true;
-// (10.48);
-// Expected;
-// false;
-
-const wholeNumber = (num) => {
-  let result = num - Math.floor(num) == 0;
-
-  if (result) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-console.log(wholeNumber(10));
-
-////////////////////////////////////////////////////
-
-// 13. Multiplication, division, and comparison operators
-// Write a function that takes two numbers, say a and b, as arguments
-// If a is smaller than b, divide a by b
-// Otherwise, multiply both numbers
-// Return the resulting value
-
-// Test Cases:
-// (10, 100)
-// Expected
-// 0.1
-// (90, 45)
-// Expected
-// 4050
-
-const divisionMultipicationFunction = (num1, num2) => {
-  if (num1 < num2) {
-    return (result1 = num1 / num2);
-  } else {
-    return (result2 = num1 * num2);
-  }
-};
-console.log(divisionMultipicationFunction(90, 45));
-
-// //////////////////////////////////////////
-//15.
-const stringFunction = (word1, word2) => {
-  if (word1 > word2) {
-    return word1 + word2;
-  } else {
-    return word2 + word1;
-  }
-};
-console.log(stringFunction(" think, therefore I am", "I"));
-
-// ///////////////////////////////////////////
-
-// 16. Split a number into its digits
-// Write a function that takes a number as argument
-// The function should split the number into its individual digits and return them in an array
-// Tipp: you might want to change the type of the number for the splitting
-
-// Test Cases:
-// (931)
-// Expected
-// [9,3,1]
-// (193278)
-// Expected
-// [1,9,3,2,7,8]
-
-const myFunction = (num) => {
-  return (numbers = [...String(num)].map(Number));
-};
-console.log(myFunction(193278));
-
-// or
-
-const numberTostring = (numbers) => {
-  let array = numbers.toString().split(""); //stringify the number, then make each digit an item in an array
-  return array.map((num) => parseInt(num)); //convert all the items back into numbers
-};
-//use the function
-console.log(numberTostring(193278));
-
-// The parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN
-
-// //////////////////////////////////////////////////
-
-// 17. Clear up the chaos behind these strings
-// It seems like something happened to these strings
-// Can you figure out how to clear up the chaos?
-// Write a function that joins these strings together such that they form the words 'Javascript', 'Countryside', and 'Downtown'!
-// You might want to apply basic JS string methods such as replace(), split(), slice() etc
-
-// Test Cases:
-// ('java', 'tpi%rcs')
-// Expected
-// 'Javascript'
-// ('c%ountry', 'edis')
-// Expected
-// 'Countryside'
-// ('down', 'nw%ot')
-// Expected
-// 'Downtown'
-
-const addToString = (str1, str2) => {
-  const string2 = str2.split("").reverse().join("").replace("%", "");
-  const string1 = str1.replace("%", "");
-  // const string2 = str2.replace("%", "").reverse('');
-  return (result = string1 + string2);
-};
-
-console.log(addToString("java", "tpi%rcs"));
-
-// /////////////////////////////////////////
-
-//18. Write a function that takes two numbers, say x and y, as arguments
-// Check if x is divisible by y
-// If yes, return x
-// If not, return the next higher natural number that is divisible by y
-
-//Test Cases:
-// (1, 23)
-// Expected
-// 23
-// (23, 23)
-// Expected
-// 23
-// (7, 3)
-// Expected
-// 9
-// (-5, 7)
-// Expected
-// 0
-
-const findDivisibleNumber = (number, divider) => {
-  while (number % divider !== 0) {
-    number++;
-  }
-  return number;
-};
-const result = findDivisibleNumber(7, 3);
-console.log(result);
-
-//////////////////////////////////////////
-
-// 19. Round a number to 2 decimal places
-// Write a function that takes a number as argument
-// Round the number to the 2nd digit after the comma
-// Return the rounded number
-
-// Input Output:
-// input:2.12397
-// output: 2.12
-// input: 3.136
-// output: 3.14
-// input: 26.1379
-// output: 26.14
-
-const roundNumber = (num) => {
-  return (number = num.toFixed(2));
-};
-const result = roundNumber(26.1379);
-console.log(result);
-
-///////////////////////////////////////////////////////////
-
-// 20. Comparison operators, strict equality
-// Write a function that takes two values, say a and b, as arguments
-// Return true if the two values are equal and of the same type
-
-// Test Cases:
-// (2, 3)
-// Expected
-// false
-// (3, 3)
-// Expected
-// true
-// (1, '1')
-// Expected
-// false
-// ('10', '10')
-// Expected
-// true
-
-const compareTwoNumber = (num1, num2) => {
-  while (num1 === num2) {
-    return true;
-  }
-  return false;
-};
-const result = compareTwoNumber("10", "10");
-console.log(result);
+})
+console.log(trueArray)
 
 //or
-const compareTwoNumber = (num1, num2) => {
-  return num1 === num2 ? true : false;
-};
-const result = compareTwoNumber("10", "10");
-console.log(result);
+const trueArray = mixdArray.filter(Boolean);
+console.log(trueArray);
 
-///////////////////////////////////////////////////
 
-// 21. Find the correct word by incrementing letters in alphabet
-// Write a function that takes a string as argument
-// As it is, the string has no meaning
-// Increment each letter to the next letter in the alphabet
-// Return the correct word
+//13. Object theke falsy value baad dibo.
+const obj = {
+   a:'lws',
+   b: undefined,
+   c: 'learn with sumit',
+   d: false,
+   e: "",
+   f: 'apple',
+   g: 40,
+   h: "k",
+   i: true,
+   j: "Thanks All",
+   k: NaN
+}
 
-// Test Cases:
-// ('bnchmf')
-// Expected
-// 'coding'
-// ('bgddrd')
-// Expected
-// 'cheese'
-// ('sdrshmf')
-// Expected
-// 'testing'
+const trueValue = (obj) => {
+    for (element in obj) {
+      if (!obj[element]) {
+        delete obj[element]
+      } 
+    }
+    return obj;
+}
 
-const findCorrectWord = (str) => {};
-const result = findCorrectWord("bnchmf");
-console.log(result);
-
-///////////////////////////////////////////////////////////////////////////
-
-// 22. Check if all array elements are equal
-// Write a function that takes an array as argument
-// It should return true if all elements in the array are equal
-// It should return false otherwise
-
-// Input, output:
-// ([true, true, true, true]);
-// true;
-// (["test", "test", "test"]);
-// true;
-// ([1, 1, 1, 2]);
-// false;
-// (["10", 10, 10, 10]);
-// false;
-
-const arrayEqualRoNot = (arr) => arr.every((v) => v === arr[0]);
-const result = arrayEqualRoNot([true, true, true, true]);
-console.log(result);
-console.log(typeof result); //boolean
-
-// every((element, index, array) => { ... } )
-// every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.
-
-//////////////////////////////////////////////////////////////////////////////////
-
-// 23. Sort an array of numbers in descending order
-// Write a function that takes an array of numbers as argument
-// It should return an array with the numbers sorted in descending order
-// Test Cases:
-// myFunction([1,3,2])
-// Expected
-// [3,2,1]
-// myFunction([4,2,3,1])
-// Expected
-// [4,3,2,1]
-
-const arraySort = (arr) => {
-  return arr.sort().reverse();
-};
-
-const result = arraySort([4, 2, 3, 1]);
-console.log(result);
-
-// 24. Sort an array of strings alphabetically
-// Write a function that takes an array of strings as argument
-// It should return the array with its values sorted alphabetically
-
-// Test Cases:
-// myFunction(['b', 'c', 'd', 'a'])
-// Expected
-// ['a', 'b', 'c', 'd']
-// myFunction(['z', 'c', 'd', 'a', 'y', 'a', 'w'])
-// Expected
-// ['a', 'a', 'c', 'd', 'w', 'y', 'z']
-
-const arraySort = (arr) => {
-  return arr.sort();
-};
-
-const result = arraySort(["z", "c", "d", "a", "y", "a", "w"]);
-console.log(result);
-
-///////////////////////////////////////////////////////////////////
-
-// 25. Return the average of an array of numbers
-// Write a function that takes an array of numbers as argument
-// It should return the average of the numbers
-
-// Test Cases:
-// myFunction([10, 100, 40]);
-// Expected;
-// 50;
-// myFunction([10, 100, 1000]);
-// Expected;
-// 370;
-// myFunction([-50, 0, 50, 200]);
-// Expected;
-// 50;
-
-const findAverage = (arr) => {
-  const result =
-    arr.reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
-      0
-    ) / arr.length;
-  return result;
-};
-console.log(findAverage([10, 100, 40]));
-
-//syntax: reduce((previousValue, currentValue, currentIndex, array) => { ... } )
-
-//or ai rokonm code korle = NaN
-// const grades = [80, 77, 88, 95, 68];
-// let avg = (grades / grades.length) * grades.length;
-// console.log(avg); // ai tar output NaN
-
-//////////////////////////////////////////////////////////////////////
+console.log(trueValue(obj))
